1601df1274dd70caf575968bafe1b75d68582a52
sumaya <202200100@uaeu.ac.ae>
2026-03-05 12:53:34 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# Years supported by your app
+YEARS = [2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024]
+
+# Default location (Abu Dhabi example)
+LOCATION_LAT = 24.4539
+LOCATION_LON = 54.3773
+LOCATION_NAME = "Abu Dhabi"
