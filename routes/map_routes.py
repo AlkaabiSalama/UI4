6b7efe0406d0b21a5f98c67c4764736bae6e18bc
Salama <alkaabi.salama001@gmail.com>
2026-04-16 03:55:38 +0400

    Enable prediction tab image mode from outputs

--- routes/map_routes.py
+++ routes/map_routes.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
+from fastapi.responses import FileResponse
 
 from schemas.requests import MapRequest, VideoRequest
 from services.map_service import map_config as map_config_service
+from services.map_service import prediction_image_path, prediction_options
 from services.video_service import timeseries_video as timeseries_video_service
 
 
@@ -16,3 +18,14 @@ def map_config(req: MapRequest):
 @router.post("/timeseries-video")
 def timeseries_video(req: VideoRequest):
     return timeseries_video_service(req)
+
+
+@router.get("/prediction-options")
+def get_prediction_options():
+    return prediction_options()
+
+
+@router.get("/prediction-image")
+def get_prediction_image(region: str = Query(...), date: str = Query(...)):
+    path = prediction_image_path(region, date)
+    return FileResponse(path)
